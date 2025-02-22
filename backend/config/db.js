0d58ff327597e88d13f2218b2d4dad3b946c300f
Ashwani Kumar <itsashu268@gmail.com>
2025-02-23 02:41:56 +0530

    db file updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT || 5432,
 });
 
 export default pool;
